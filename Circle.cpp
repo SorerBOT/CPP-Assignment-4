fa1a12ef4598e96cc09d0f0a1864b141c807659d
SorerBOT <alonfiller@gmail.com>
2022-06-01 18:38:45 +0300

    (Patch) implemented operators Circle class

--- Circle.cpp
+++ Circle.cpp
@@ -17,3 +17,12 @@ double Circle::getRadius() const { return this->m_radius; }
 void Circle::setRadius(double r) { this->m_radius = r; }
 int Circle::calcArea() const { return (int) (M_PI * pow(this->getRadius(), 2)); }
 int Circle::calcPerimeter() const { return (int) (M_PI * 2 * this->getRadius()); }
+
+const Circle &Circle::operator=(const Circle &other) { this->setRadius(other.getRadius()); this->setShape(other.getName(), other.getCenter()); return *this; }
+const Circle &Circle::operator=(const char *name) { this->setName(name); return *this; }
+
+Circle operator+(int r, const Circle &other) {
+    Circle circle(r, other.getName());
+    circle.setShape(other.getName(), other.getCenter());
+    return circle;
+}
